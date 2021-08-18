6c05946fd447d0c467e6c01ceb3a3b4a197680be
toufiq007 <t.limon2342@gmail.com>
2021-08-18 14:48:24 +0600

    added increase decrease and update price functionality

--- js/app.js
+++ js/app.js
@@ -1,21 +1,30 @@
 // increase the case
 document.getElementById("casePlus").addEventListener("click",function(){
-  changeItemValue("caseNumber",true)
+  updateCaseNumber("caseNumber",true)
 });
 
 document.getElementById("caseMinus").addEventListener("click",function(){
-  let decreseItem = changeItemValue("caseNumber",false)
+  let decreseItem = updateCaseNumber("caseNumber",false)
   console.log(decreseItem);
 });
 
-function changeItemValue(inputId,isAdd){
+function updateCaseNumber(inputId,isAdd){
   let input = document.getElementById(inputId);
   let inputValue = input.value;
+  let price = 59;
+  let casePrice = document.getElementById("casePrice");
+  console.log(casePrice.innerText);
+
   if (isAdd==true){
     input.value = parseInt(inputValue) + 1;
+    price = price * input.value
+    casePrice.innerText = price;
+    // console.log(price)
   }
   else if (inputValue > 0){
     let decreseItem = input.value = parseInt(inputValue) - 1
+    price = price * input.value
+    casePrice.innerText = price;
   }
   return input.value;
 }
