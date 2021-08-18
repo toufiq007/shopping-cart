75b6417d3680b27797f6218ab927d9108a1424af
toufiq007 <t.limon2342@gmail.com>
2021-08-18 14:20:51 +0600

    increase and decrease functionality added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log('I am in a separate file')
+// increase the case
+document.getElementById("casePlus").addEventListener("click",function(){
+  changeItemValue("caseNumber",true)
+});
+
+document.getElementById("caseMinus").addEventListener("click",function(){
+  let decreseItem = changeItemValue("caseNumber",false)
+  console.log(decreseItem);
+});
+
+function changeItemValue(inputId,isAdd){
+  let input = document.getElementById(inputId);
+  let inputValue = input.value;
+  if (isAdd==true){
+    input.value = parseInt(inputValue) + 1;
+  }
+  else if (inputValue > 0){
+    let decreseItem = input.value = parseInt(inputValue) - 1
+  }
+  return input.value;
+}
+
+// decrease the case
