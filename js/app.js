377eeb63d790bb7c9b9285b22560b41ebdcbf951
toufiq007 <t.limon2342@gmail.com>
2021-08-18 23:47:23 +0600

    complete shopping cart

--- js/app.js
+++ js/app.js
@@ -1,33 +1,36 @@
 // increase the case
 document.getElementById("casePlus").addEventListener("click",function(){
   let increaseItem = updateCaseNumber("caseNumber",true,59,"casePrice")
-  let total = subTotal("casePrice","productPrice","subTotal")
-  console.log(total)
 });
 
 document.getElementById("caseMinus").addEventListener("click",function(){
   let decreseItem = updateCaseNumber("caseNumber",false,59,"casePrice")
-  let total = subTotal("casePrice","productPrice","subTotal")
 });
 
 document.getElementById("productPlus").addEventListener("click",function(){
   let updatePhonePrice = updateCaseNumber("productUpdate",true,1219,"productPrice")
-  let total = subTotal("casePrice","productPrice","subTotal")
 })
 
 document.getElementById("productMinus").addEventListener("click",function(){
   let updatePhonePrice = updateCaseNumber("productUpdate",false,1219,"productPrice")
-  let total = subTotal("casePrice","productPrice","subTotal")
 })
 
-function subTotal(intputId1,inputId2,targetId){
+function subTotal(intputId1,inputId2,targetId,taxId,grandTotal){
   let product1 = document.getElementById(intputId1).innerText;
   let product2 = document.getElementById(inputId2).innerText;
   let target   = document.getElementById(targetId);
+  let taxTotal = document.getElementById(taxId);
+  let finalTotal = document.getElementById(grandTotal);
 
-  // console.log(product2,product1,target)
+  // calculate total product price + case price
   let totalSum = parseInt(product1) + parseInt(product2);
   target.innerText = totalSum;
+  // calculate totalprice + tax price
+  let tax = parseInt(totalSum * 0.1)
+  taxTotal.innerText = tax
+  // calculate grandTotal price
+  finalTotal.innerText = totalSum + tax;
+  console.log(tax)
   return totalSum;
 }
 
@@ -36,23 +39,21 @@ function updateCaseNumber(inputId,isAdd,productPrice,targetId){
   let productTarget = document.getElementById(targetId);
   let inputValue = input.value;
   let price = productPrice;
-  // let casePrice = document.getElementById(inputId);
-  // console.log(casePrice.innerText);
 
   if (isAdd==true){
     input.value = parseInt(inputValue) + 1;
     price = price * input.value
     productTarget.innerText = price
-    return productTarget.innerText
+    // return productTarget.innerText
+    subTotal("casePrice","productPrice","subTotal","taxTotal","grandTotal")
     // console.log(price)
   }
   else if (inputValue > 0){
     let decreseItem = input.value = parseInt(inputValue) - 1
     price = price * input.value
     productTarget.innerText = price
-    return productTarget.innerText
+    // return productTarget.innerText
+    subTotal("casePrice","productPrice","subTotal","taxTotal","grandTotal")
   }
-  return input.value;
+  
 }
-
-// decrease the case
