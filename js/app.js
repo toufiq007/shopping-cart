80c2a7b2ab4e0776ddf83afc31c465ea4a83187a
toufiq007 <t.limon2342@gmail.com>
2021-08-18 20:42:39 +0600

    create quantity and update price function added

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
 // increase the case
 document.getElementById("casePlus").addEventListener("click",function(){
-  updateCaseNumber("caseNumber",true)
+  updateCaseNumber("caseNumber",true,59,"casePrice")
 });
 
 document.getElementById("caseMinus").addEventListener("click",function(){
-  let decreseItem = updateCaseNumber("caseNumber",false)
+  let decreseItem = updateCaseNumber("caseNumber",false,59,"casePrice")
   console.log(decreseItem);
 });
 
-function updateCaseNumber(inputId,isAdd){
+function updateCaseNumber(inputId,isAdd,productPrice,targetId){
   let input = document.getElementById(inputId);
+  let productTarget = document.getElementById(targetId);
   let inputValue = input.value;
-  let price = 59;
-  let casePrice = document.getElementById("casePrice");
+  let price = productPrice;
+  let casePrice = document.getElementById(inputId);
   console.log(casePrice.innerText);
 
   if (isAdd==true){
     input.value = parseInt(inputValue) + 1;
     price = price * input.value
-    casePrice.innerText = price;
+    productTarget.innerText = price
     // console.log(price)
   }
   else if (inputValue > 0){
     let decreseItem = input.value = parseInt(inputValue) - 1
     price = price * input.value
-    casePrice.innerText = price;
+    productTarget.innerText = price
   }
   return input.value;
 }
